ed154724b44d1adb5a4d320efe8acedced57817d
DanielRailean <daniel.railean1@gmail.com>
2024-06-21 12:06:50 +0200

    Update handler.lua

--- kong/plugins/jwt-oidc-validate/handler.lua
+++ kong/plugins/jwt-oidc-validate/handler.lua
@@ -33,6 +33,10 @@ function jwt_oidc_validate:access(conf)
       table.insert(words, w)
     end
 
+    if not words[2] then
+      return kong.response.exit(401, { title = "Unauthorized" })
+    end
+
     local jwt_obj = jwt:load_jwt(words[2])
     local payload = jwt_obj.payload
     local issuer = payload.iss
